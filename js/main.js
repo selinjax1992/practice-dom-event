f42ad60a463605c46764fbaabacd40b36964d27a
SelinJax <seljax92@gmail.com>
2026-09-12 13:54:33 +0300

    -Alert Box with Dismiss Option

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ const refs = {
   productTitle: document.querySelector("#product-title"),
   actionBtn: document.querySelector("#action-btn"),
   todoList: document.querySelector("#todo-list"),
+  alertBox: document.querySelector("#alert-box"),
+  closeAlertBtn: document.querySelector("#close-alert-btn"),
+  body: document.querySelector("body"),
 };
 refs.productImg.src = "https://picsum.photos/200/300";
 refs.productTitle.textContent = "Awesome Tech Gadget";
@@ -28,3 +31,17 @@ tasks.forEach((task) => {
   li.classList.add("todo-item");
   refs.todoList.append(li);
 });
+
+// ==========================================
+// TASK 3: Alert Box with Dismiss Option
+// ==========================================
+// ~ Version 1: Using remove() method
+// const onCloseBtnClick = () => {
+//   refs.alertBox.remove();
+// };
+// ~ Version 2: Using removeChild() method
+const onCloseBtnClick = () => {
+  refs.body.removeChild(refs.alertBox);
+};
+
+refs.closeAlertBtn.addEventListener("click", onCloseBtnClick);
