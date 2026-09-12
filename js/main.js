73367e2e0f326df260dab6e91727e2438e82aeb6
SelinJax <seljax92@gmail.com>
2026-09-12 14:58:42 +0300

    Interactive Coordinates Tracker add mouseleave event

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// ==========================================
+// TASK 1: Interactive Coordinates Tracker
+// ==========================================
 const refs = {
   coordsBox: document.querySelector("#coords-box"),
   coordsText: document.querySelector("#coords-text"),
@@ -7,4 +10,8 @@ refs.coordsBox.addEventListener("mousemove", (event) => {
   const { offsetX, offsetY } = event;
 
   refs.coordsText.textContent = `Move your mouse here to track coordinates: X:${offsetX} Y: ${offsetY}`;
+});
+refs.coordsBox.addEventListener("mouseleave", () => {
+  refs.coordsText.textContent =
+    "Move your mouse here to track coordinates: X:0 Y: 0";
 });
