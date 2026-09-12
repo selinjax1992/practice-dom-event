219d0838462341b0344cc157f3b953bda5d1f556
SelinJax <seljax92@gmail.com>
2026-09-12 15:06:58 +0300

    Custom Context Menu Preventer

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 // ==========================================
-// TASK 1: Interactive Coordinates Tracker
+// REFERENCES
 // ==========================================
 const refs = {
   coordsBox: document.querySelector("#coords-box"),
   coordsText: document.querySelector("#coords-text"),
+  menuTitle: document.querySelector("#menu-title"),
 };
+
+// ==========================================
+// TASK 1: Interactive Coordinates Tracker
+// ==========================================
 
 refs.coordsBox.addEventListener("mousemove", (event) => {
   const { offsetX, offsetY } = event;
@@ -14,4 +19,13 @@ refs.coordsBox.addEventListener("mousemove", (event) => {
 refs.coordsBox.addEventListener("mouseleave", () => {
   refs.coordsText.textContent =
     "Move your mouse here to track coordinates: X:0 Y: 0";
+});
+
+// ==========================================
+// TASK 2: Custom Context Menu Preventer
+// ==========================================
+
+refs.menuTitle.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
+  alert("Custom actions will be here soon!");
 });
