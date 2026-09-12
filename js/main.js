07a83c8c7d28d95bfe667039c51ca8b55fc2cd4c
SelinJax <seljax92@gmail.com>
2026-09-12 17:17:24 +0300

    task 1 clean input

--- js/main.js
+++ js/main.js
@@ -4,12 +4,15 @@
 const refs = {
   userNameInput: document.querySelector("#username-input"),
   userNameError: document.querySelector("#username-error"),
+  passwordInput: document.querySelector("#password-input"),
 };
 // ==========================================
 // TASK-1: Username Validator
 // ==========================================
 refs.userNameInput.addEventListener("input", () => {
-  if (refs.userNameInput.value.length < 5) {
+  if (refs.userNameInput.value === "") {
+    refs.userNameError.textContent = "";
+  } else if (refs.userNameInput.value.length < 5) {
     refs.userNameError.textContent =
       "Username must be at least 5 characters long";
     refs.userNameInput.style.color = refs.userNameError.style.color = "red";
@@ -18,4 +21,14 @@ refs.userNameInput.addEventListener("input", () => {
     refs.userNameInput.style.color = "black";
     refs.userNameError.style.color = "green";
   }
+});
+// ==========================================
+// TASK-2: Focus Highlighter
+// ==========================================
+
+refs.passwordInput.addEventListener("focus", () => {
+  refs.passwordInput.style.borderColor = "red";
+});
+refs.passwordInput.addEventListener("blur", () => {
+  refs.passwordInput.style.borderColor = "grey";
 });
